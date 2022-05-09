6196afa99d206e7a65025a78bf60bc978a8614b1
note-jeffy <931312254@qq.com>
2022-05-09 14:25:47 +0800

    练习 for 循环、数组、for-range

--- mon-5/day-05-09/03-for-loop/main.go
+++ mon-5/day-05-09/03-for-loop/main.go
@@ -1,5 +1,5 @@
 /**
- * 练习 for 循环
+ * 练习 for 循环、数组、for-range
  */
 
 package main
@@ -9,9 +9,17 @@ import (
 )
 
 func main() {
-	fmt.Println("hello_world")
+	fmt.Println("No.1 test")
 
 	for i, j := 1, 1; i <= 5 && j <= 5; i, j = i+1, j+1 {
 		println(i, "  ", j)
 	}
+
+	fmt.Println("No.2 test")
+	// 这里涉及到了数组
+	nums := []int{1, 5, 2, 3, 7, 3, 4, 8}
+	for i, num := range nums {
+		fmt.Println("index: ", i, "\tvalue: ", num)
+	}
+
 }
