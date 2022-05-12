d125275a258e64259ec9af444a94ad4a76200919
note-jeffy <931312254@qq.com>
2022-05-12 13:26:36 +0800

    方法和接收者

--- mon-5/day-05-12/01-方法/main.go
+++ mon-5/day-05-12/01-方法/main.go
@@ -13,7 +13,7 @@ type person struct {
 	name string
 }
 
-// 这里的传参必须是指针类型， 不然只能在函数局部改变变量值
+// 这里的传参必须是指针类型， 不然修改操作只是针对副本，无法修改接收者变量本身。
 func (p *person) set_age(age int) {
 	p.age = 18
 	fmt.Println(p)
