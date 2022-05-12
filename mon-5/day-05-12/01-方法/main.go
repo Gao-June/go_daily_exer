b96b7ef3369f99c40063f5943380656e3a8b292a
note-jeffy <931312254@qq.com>
2022-05-12 13:25:55 +0800

    方法和接收者

---
+++ mon-5/day-05-12/01-方法/main.go
@@ -0,0 +1,32 @@
+/*
+方法和接收者
+	方法限定了该函数的调用者
+	只有满足类型的 调用者才可以调用该方法
+*/
+
+package main
+
+import "fmt"
+
+type person struct {
+	age  int
+	name string
+}
+
+// 这里的传参必须是指针类型， 不然只能在函数局部改变变量值
+func (p *person) set_age(age int) {
+	p.age = 18
+	fmt.Println(p)
+}
+
+func main() {
+	wang := person{
+		age:  100,
+		name: "小王",
+	}
+
+	fmt.Println(wang)
+	wang.set_age(20)
+	fmt.Println(wang)
+
+}
