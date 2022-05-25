67e11e0d93bdf846088c753ad1c3d2ee9b48a993
lib_jeffygao <gaojune_better@qq.com>
2022-05-25 22:14:19 +0800

    《项目》01 - 网络编程

---
+++ 一些小项目学习/01-网络编程/TCP编程/client/main.go
@@ -0,0 +1,56 @@
+/*
+	TCP客户端程序的处理流程：
+		1.建立与服务端的链接
+    	2.进行数据收发
+    	3.关闭链接
+
+以下是 TCP client 端代码实现：
+*/
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "127.0.0.1:10010")
+	if err != nil {
+		fmt.Println("err: ", err)
+		return
+	}
+
+	// 关闭连接
+	defer conn.Close()
+
+	inputReader := bufio.NewReader(os.Stdin)
+
+	for {
+		input, _ := inputReader.ReadString('\n') // 读取用户输入
+		inputInfo := strings.Trim(input, "\r\n")
+
+		// 输入 Q 就退出
+		if strings.ToUpper(inputInfo) == "Q" {
+			return
+		}
+
+		// 发送数据
+		_, err := conn.Write([]byte(inputInfo))
+		if err != nil {
+			return
+		}
+
+		buf := [512]byte{}
+		n, err := conn.Read(buf[:])
+		if err != nil {
+			fmt.Println("recv failed, err: ", err)
+			return
+		}
+
+		fmt.Println(string(buf[:n]))
+	}
+}
---
+++ 一些小项目学习/01-网络编程/TCP编程/server/main.go
@@ -0,0 +1,60 @@
+/*
+	TCP服务端程序的处理流程：
+		1 - 监听端口
+		2 - 接收客户端请求建立连接
+		3 - 创建 goroutine 处理连接
+
+以下是 TCP server 端代码实现：
+*/
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+// 处理函数
+func process(conn net.Conn) {
+	// 关闭链接
+	defer conn.Close()
+
+	// 死循环，一直监听
+	for {
+		reader := bufio.NewReader(conn)
+		var buf [128]byte
+
+		// 读取数据
+		n, err := reader.Read(buf[:])
+		if err != nil {
+			fmt.Println("read from client failed, err: ", err)
+			break
+		}
+
+		recvStr := string(buf[:n])
+		fmt.Println("收到 client 端发来的数据: ", recvStr)
+
+		// 发送数据
+		conn.Write([]byte(recvStr))
+	}
+}
+
+// 主函数
+func main() {
+	listen, err := net.Listen("tcp", "127.0.0.1:10010")
+	if err != nil {
+		fmt.Println("listen failed, err: ", err)
+		return
+	}
+
+	for {
+		// 建立连接
+		conn, err := listen.Accept()
+		if err != nil {
+			fmt.Println("accept failed, err: ", err)
+			continue
+		}
+		go process(conn)
+	}
+}
