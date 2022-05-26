743d96ab0cd19dcf00fa5253a5110f1841443676
lib_jeffygao <gaojune_better@qq.com>
2022-05-26 16:10:57 +0800

    《项目》01 - 04 - socket编程

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module go_daily_exer
 
 go 1.17
+
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+)
---
+++ 一些小项目学习/01-网络编程/04 - socket编程/server.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+	go h.run()
+	router.HandleFunc("/ws", myws)
+	if err := http.ListenAndServe("10.16.48.232:8080", router); err != nil {
+		fmt.Println("err:", err)
+	}
+}
