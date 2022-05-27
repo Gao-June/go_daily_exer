5e2dab817140447d573cc54a116e46115079bdda
lib_jeffygao <gaojune_better@qq.com>
2022-05-27 10:23:05 +0800

    练习使用 time 时间类型

--- mon-5/day-05-27/01-time/main.go
+++ mon-5/day-05-27/01-time/main.go
@@ -8,6 +8,7 @@ import (
 )
 
 func main( ){
+// 简单表述时间
 	now := time.Now()
 	fmt.Println("time now: ", now)
 
@@ -20,5 +21,15 @@ func main( ){
 	fmt.Println("time: ", year, "-", month,
 				 ".", day, "-", hour, 
 				 ":", minute, ":", second)
+
+// 记录时间差
+	time_begin := time.Now()
+	var num int = 1		// 添加操作，让时间长一点
+	for i := 1; i < 1000000; i ++{
+		num++
+	}
+	time_end  := time.Now()
+	time_dur := time_end.Sub(time_begin)
+	fmt.Println("时间间隔：", time_dur)
 
 }
