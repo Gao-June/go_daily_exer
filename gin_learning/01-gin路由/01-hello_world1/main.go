8af24b63241d799d192dde20faf1a19e77634bdd
lib_jeffygao <gaojune_better@qq.com>
2022-05-28 14:51:10 +0800

    1

--- gin_learning/01-gin路由/01-hello_world1/main.go
+++ gin_learning/01-gin路由/01-hello_world1/main.go
@@ -10,16 +10,23 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func this_fun(context *gin.Context) {
+	fmt.Println("请求路径", context.FullPath())
+	context.Writer.Write([]byte("Hello, gin\n"))
+}
+
 func main() {
 	// 创建 默认路由
 	engine := gin.Default()
 
 	// 绑定路由规则，定义执行的函数
 	// gin.Context 封装了 request 和 response
-	engine.GET("/hello", func(context *gin.Context) {
-		fmt.Println("请求路径", context.FullPath())
-		context.Writer.Write([]byte("Hello, gin\n"))
-	})
+	// 这里的 func(){...} 类似lambda，其实可以再写一个函数，然后把函数名放在这里。
+	// engine.GET("/hello", func(context *gin.Context) {
+	// 	fmt.Println("请求路径", context.FullPath())
+	// 	context.Writer.Write([]byte("Hello, gin\n"))
+	// })
+	engine.GET("/hello", this_fun)
 
 	// 监听端口，默认是在 8080
 	// Run 可以修改端口
